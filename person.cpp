fdd4888597bb1319ae544a368b3a4c035102d1aa
landsquid9 <daniel.bandfield@ntlworld.com>
2017-08-28 12:14:40 +0000

    person

--- person.cpp
+++ person.cpp
@@ -1,11 +1,74 @@
 #include "person.h"
 
-Person::Person()
+Person::Person(cv::Rect _box)
 {
-    //ctor
+    disp = true;
+    remove = false;
+    red = false;
+
+    displayTimer = 0;
+    displayMaxTimer = 4;
+
+    mainBox = _box
+    calcBoxes();
 }
 
 Person::~Person()
 {
     //dtor
+}
+
+bool Person::update(cv::Rect otherBox)
+{
+    red = !red;
+    bool same = false;
+    if((mainBox & otherBox).area() > 50)
+    {
+        mainBox = otherBox;
+        calcBoxes();
+        same = true;
+    }
+
+
+    displayTimer ++;
+    if(displayTimer > displayTimrerMax) disp = false;
+
+    return same;
+}
+
+void Person::display(cv::Mat &im)
+{
+    bool tmpRed = red;
+    if(disp)
+    {
+        for(int i = 0; i < boxes.size(); i ++)
+        {
+
+            if(tmpRed)
+            {
+                cv::rectangle(im, boxes[i], cv::Scalar(0, 0, 255) );
+            }
+            else
+            {
+                cv::rectangle(im, boxes[i], cv::Scalar(50, 50, 255) );
+
+            }
+            tmpRed = !tmpRed;
+        }
+    }
+}
+
+void Person::calcBoxes()
+{
+    boxes.clear();
+    int inc = 3;
+    cv::Rect tmpBox = cv::Rect(mainBox.x, mainBox.y, mainBox.width, mainBox.height);
+    while(tmpBox. width > 0 && tmpBox.height > 0)
+    {
+        tmpBox.x += inc;
+        tmpBox.y += inc;
+        tmpBox.width -= (inc * 2);
+        tmpBox.height -= (inc * 2);
+        boxes.push_back(tmpBox);
+    }
 }
--- person.h
+++ person.h
@@ -1,9 +1,35 @@
+#pragma once
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/bgsegm.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+
+#include <vector>
+#include <iostream>
+#include <string>
 
 class Person
 {
     public:
-        Person();
+        Person(cv::Rect _box);
         virtual ~Person();
+        bool update(cv::Rect otherBox);
+        void display(cv::Mat &im);
+
+        bool getRemove(){return remove;}
     protected:
+        cv::Rect mainBox;
+        std::vector<cv::Rect> boxes;
+        bool remove;
+        bool disp;
+        bool red;
+
+        int displayTimer;
+        int displayMaxTimer;
+
+        void calcBoxes();
     private:
 };
